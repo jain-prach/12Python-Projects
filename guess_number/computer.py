6e06c240eb7810bdc048a4dd4451d4dbbbe65d6e
jain-prach <76729905+jain-prach@users.noreply.github.com>
2022-06-19 19:10:24 +0530

    Add files via upload

---
+++ guess_number/computer.py
@@ -0,0 +1,19 @@
+import random
+def computer_guess(x):
+    low=1
+    high=x
+    feedback=''
+    while feedback!='c':
+        if low!=high:
+            guess=random.randint(low,high)
+        else:
+            guess=low #can also be high as low=high
+        feedback=input(f'Is {guess} too high(h), too low(l) or correct(c)??')
+        if feedback=='h':
+            high=guess-1
+        elif feedback=='l':
+            low=guess+1
+            
+    print(f"The computer has guessed your number, {guess} correctly");
+
+computer_guess(1000)
